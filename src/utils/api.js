ae5aa1fb06d204c5352206b4684a968bf9849cf7
Rami Taibah <taibah@gmail.com>
2017-09-02 02:46:06 +0300

    API calls done

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,43 +7,36 @@ const headers = {
 };
 
 // GET METHODS
-
-// Gets all categories in Readable
 export const getCategories = () =>
   fetch(`${api}/categories`, {headers})
     .then(response => response.json())
     .then(data => data.categories)
     .catch(e => console.log('error, e'));
 
-// Gets all posts in a category
 export const getCategoryPosts = category =>
   fetch(`${api}/${category}/posts`, {headers})
     .then(response => response.json())
     .then(data => data)
     .catch(e => console.log('error, e'));
 
-// Get all posts
 export const getPosts = () =>
   fetch(`${api}/posts`, {headers})
     .then(response => response.json())
     .then(data => data)
     .catch(e => console.log('error, e'));
 
-// Get a single Post
 export const getSinglePost = id =>
   fetch(`${api}/posts/${id}`, {headers})
     .then(response => response.json())
     .then(data => data)
     .catch(e => console.log('error, e'));
 
-// Get all comments in a post
 export const getComments = id =>
   fetch(`${api}/posts/${id}/comments`, {headers})
     .then(response => response.json())
     .then(data => data)
     .catch(e => console.log('error, e'));
 
-// Get comment details
 export const commentDetails = id =>
   fetch(`${api}/comments/${id}`, {headers})
     .then(response => response.json())
@@ -51,7 +44,6 @@ export const commentDetails = id =>
     .catch(e => console.log('error, e'));
 
 // POST METHODS
-
 export const newPost = obj =>
   fetch(`${api}/posts`, {
     method: 'POST',
@@ -68,7 +60,7 @@ export const newPost = obj =>
     })
     .catch(e => console.log('error, e'));
 
-export const vote = (vote, id) =>
+export const votePost = (vote, id) =>
   fetch(`${api}/posts/${id}`, {
     method: 'POST',
     headers: {
@@ -78,8 +70,101 @@ export const vote = (vote, id) =>
     },
     body: JSON.stringify(vote),
   })
-    .then(console.log(JSON.stringify(vote)))
     .then(response => response.json())
+    .then(data => {
+      return data;
+    })
+    .catch(e => console.log('error, e'));
+
+export const voteComment = (vote, id) =>
+  fetch(`${api}/comments/${id}`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': type,
+      Authorization: token,
+    },
+    body: JSON.stringify(vote),
+  })
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    })
+    .catch(e => console.log('error, e'));
+export const newComment = obj =>
+  fetch(`${api}/comments`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': type,
+      Authorization: token,
+    },
+    body: JSON.stringify(obj),
+  })
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    })
+    .catch(e => console.log('error, e'));
+
+// PUT METHODS
+export const editPost = (obj, id) =>
+  fetch(`${api}/posts/${id}`, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      'Content-Type': type,
+      Authorization: token,
+    },
+    body: JSON.stringify(obj),
+  })
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    })
+    .catch(e => console.log('error, e'));
+
+export const editComment = (obj, id) =>
+  fetch(`${api}/comments/${id}`, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      'Content-Type': type,
+      Authorization: token,
+    },
+    body: JSON.stringify(obj),
+  })
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    })
+    .catch(e => console.log('error, e'));
+
+// DELETE METHODS
+
+export const deletePost = id =>
+  fetch(`${api}/posts/${id}`, {
+    method: 'DELETE',
+    headers: {
+      ...headers,
+      'Content-Type': type,
+      Authorization: token,
+    },
+  })
+    .then(data => {
+      return data;
+    })
+    .catch(e => console.log('error, e'));
+
+export const deleteComment = id =>
+  fetch(`${api}/comments/${id}`, {
+    method: 'DELETE',
+    headers: {
+      ...headers,
+      'Content-Type': type,
+      Authorization: token,
+    },
+  })
     .then(data => {
       return data;
     })
