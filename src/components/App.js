05d349cd82dffa863ec2a99a54d4679ba890ca3f
Rami Taibah <taibah@gmail.com>
2017-09-02 01:36:07 +0300

    Add POST methods

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,38 @@ class App extends Component {
   state = {
     categories: [],
     posts: [],
+    catPosts: [],
+    singlePost: [],
+    singlePostComments: [],
+    commentDetails: [],
   };
 
-  componenetDidMount() {
-    ReadableAPI.getCategories().then(response =>
-      this.setState({categories: response}),
+  componentDidMount() {
+    // QUICK and DIRTY testing for API calls
+    ReadableAPI.getCategories().then(categories => this.setState({categories}));
+    ReadableAPI.getPosts().then(posts => this.setState({posts}));
+    ReadableAPI.getCategoryPosts('redux').then(catPosts =>
+      this.setState({catPosts}),
     );
+    ReadableAPI.getSinglePost('6ni6ok3ym7mf1p33lnez').then(singlePost =>
+      this.setState({singlePost}),
+    );
+    ReadableAPI.getComments('8xf0y6ziyjabvozdd253nd').then(singlePostComments =>
+      this.setState({singlePostComments}),
+    );
+    ReadableAPI.commentDetails(
+      '894tuq4ut84ut8v4t8wun89g',
+    ).then(commentDetails => this.setState({commentDetails}));
 
-    ReadableAPI.getPosts().then(posts => this.setState(posts));
+    ReadableAPI.newPost({
+      id: Date.now(),
+      title: 'It worked! 3',
+      body: 'Hello from my JS code!',
+      voteScore: 1,
+      deleted: false,
+    });
+
+    ReadableAPI.vote({option: 'downVote'}, '8xf0y6ziyjabvozdd253nd');
   }
 
   render() {
@@ -22,7 +46,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>welcome to React</h2>
+          {this.state.categories.map(category => (
+            <li key={category.name}>{category.name}</li>
+          ))}
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,86 @@
 const api = 'http://localhost:5001';
+const token = 'code';
+const type = 'application/json';
 
 const headers = {
-  Authorization: 'rami',
+  Authorization: token,
 };
 
+// GET METHODS
+
+// Gets all categories in Readable
 export const getCategories = () =>
   fetch(`${api}/categories`, {headers})
     .then(response => response.json())
-    .then(data => data.categories);
+    .then(data => data.categories)
+    .catch(e => console.log('error, e'));
 
-export const getPosts = () => {
-  return fetch(`${api}/posts`, {headers})
+// Gets all posts in a category
+export const getCategoryPosts = category =>
+  fetch(`${api}/${category}/posts`, {headers})
     .then(response => response.json())
-    .then(data => data);
-};
+    .then(data => data)
+    .catch(e => console.log('error, e'));
+
+// Get all posts
+export const getPosts = () =>
+  fetch(`${api}/posts`, {headers})
+    .then(response => response.json())
+    .then(data => data)
+    .catch(e => console.log('error, e'));
+
+// Get a single Post
+export const getSinglePost = id =>
+  fetch(`${api}/posts/${id}`, {headers})
+    .then(response => response.json())
+    .then(data => data)
+    .catch(e => console.log('error, e'));
+
+// Get all comments in a post
+export const getComments = id =>
+  fetch(`${api}/posts/${id}/comments`, {headers})
+    .then(response => response.json())
+    .then(data => data)
+    .catch(e => console.log('error, e'));
+
+// Get comment details
+export const commentDetails = id =>
+  fetch(`${api}/comments/${id}`, {headers})
+    .then(response => response.json())
+    .then(data => data)
+    .catch(e => console.log('error, e'));
+
+// POST METHODS
+
+export const newPost = obj =>
+  fetch(`${api}/posts`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': type,
+      Authorization: token,
+    },
+    body: JSON.stringify(obj),
+  })
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    })
+    .catch(e => console.log('error, e'));
+
+export const vote = (vote, id) =>
+  fetch(`${api}/posts/${id}`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': type,
+      Authorization: token,
+    },
+    body: JSON.stringify(vote),
+  })
+    .then(console.log(JSON.stringify(vote)))
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    })
+    .catch(e => console.log('error, e'));
